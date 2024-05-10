6a05da25a22b7e2da1fc61d1eaf2885b42b089d2
7ejasVijay <7ejas.vijay@gmail.com>
2024-05-10 11:25:27 +0530

    Add read file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
+    let query: &str = &args[1];
     let file_path = &args[2];
 
     println!("searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Not able to read the file");
+    println!("{contents}");
 }
